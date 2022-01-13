cd5aa3eeb128341383988e50c338dc6afc1debdf
Vibe3 <91519745+Vibe3@users.noreply.github.com>
2022-01-14 00:31:47 +0800

    1/14
    1/14

--- KnightSoul/Assets/KnghtSoul/Assets/Scripts/DataDialogue.cs
+++ KnightSoul/Assets/KnghtSoul/Assets/Scripts/DataDialogue.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 [CreateAssetMenu(menuName ="Dialogue/Data")]
 
-public class Data : ScriptableObject
+public class DataDialogue : ScriptableObject
 {
     [Header("對話內容"), TextArea(3, 5)]
     public string[] Dialogues;
